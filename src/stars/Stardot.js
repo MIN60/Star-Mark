b2e0bcbdeec62b0671d4bdbb1f94707d52323230
MIN60 <49427080+MIN60@users.noreply.github.com>
2022-01-24 00:41:47 +0900

    ClientX,ClientY

--- src/stars/Stardot.js
+++ src/stars/Stardot.js
@@ -2,12 +2,51 @@ import React, { useState } from "react";
 
 
 
+
+const result = document.getElementById('result');  //id result 인곳에 표시함
+document.onclick = ClickScreen;
+
+function ClickScreen(evt){
+
+  //event 객체에 접근
+  evt = evt || window.event;
+  var x = 0,
+      y = 0;
+
+  //event 객체가 pageX 속성을 포함하고 있다면 pageX와 pageY 위치
+  if(evt.pageX){
+    x = evt.pageX;
+    y = evt.pageY;
+
+  //event 객체가 clientX 속성을 포함하고 있다면
+  }else if(evt.clientX){
+    var offsetX = 0,
+        offsetY = 0;
+
+    //documentElement.scrollLeft를 지원하면
+    if(document.documentElement.scrollLeft){
+      offsetX = document.documentElement.scrollLeft;
+      offsetY = document.documentElement.scrollTop;
+    }else if(document.body){
+      offsetX = document.body.scrollLeft;
+      offsetY = document.body.scrollTop;
+    }
+
+    x = evt.clientX + offsetX;
+    y = evt.clientY + offsetY;
+  }
+
+ //document.write("you clicked at x=" + x + " y=" + y);
+  result.innerHTML = `${x}px, ${y}px`;
+}
+
 const Stardot = () => {
     //Client기준 좌표
 
   return (
     <div>
       <p>Client X, Y</p>
+      <div id="result"></div>
     </div>
   );
 };
