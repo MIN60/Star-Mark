f25d47022504bf8cb57225162b2c9169fe49a788
Jongjunp <56390530+Jongjunp@users.noreply.github.com>
2022-01-24 00:55:22 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
-import { Home } from './home/index.js';
-import Login from './home/Login.js'
-import  SignUp  from './home/signUp.js';
-import Stardot from './stars/Stardot.js';
+import { Intro } from './Intro/Intro.js';
 
 
 function App() {
@@ -13,10 +10,11 @@ function App() {
       <div>
         <Router>
           <Routes>
-              <Route exact path='/' element={<Home />}></Route>
+            <Route exact path='/' element={<Intro />}></Route>
+              {/*<Route exact path='/' element={<Home />}></Route>
               <Route path='/Login' element={<Login />}></Route>
               <Route path='/SignUp' element={<SignUp />}></Route>
-              <Route path='/Stardot' element={<Stardot />}></Route>
+              <Route path='/Stardot' element={<Stardot />}></Route>*/}
           </Routes>
         </Router>
       </div>
--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-//import { Provider } from 'react-redux';
-//import { createStore } from 'redux';
-//import rootReducer from './modules/rootReducer'
 import App from'./App'
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-
-//const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
