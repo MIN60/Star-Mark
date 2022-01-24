7f469d3f7ae6bbc9a34ebdcf2c5d39d07eb0f832
MIN60 <49427080+MIN60@users.noreply.github.com>
2022-01-24 17:05:29 +0900

    Connect DB

--- src/stars/Stardot.js
+++ src/stars/Stardot.js
@@ -1,17 +1,37 @@
-import React, { useState } from "react";
+import React, { memo, useState } from "react";
 import './Stardot.css';
+import TextField from '@mui/material/TextField';
+import axios from "axios";
 
-const result = document.getElementById('result');
+
+axios.defaults.withCredentials = true;
+axios.defaults.xsrfCookieName = "csrftoken";
+axios.defaults.xsrfHeaderName = "X-CSRFToken";
 
 document.onclick = ScreenClick;
 
+var x = 0, y = 0;
+
+
 function ScreenClick(evt){
+
+    /*
+    const email = document.getElementsByName('email')[0].value.trim();
+    const link = document.getElementsByName('link')[0].value.trim();
+    const attr = document.getElementsByName('attr')[0].value.trim();
+    const memo = document.getElementsByName('memo')[0].value.trim();
+    */
+
+    const email = 'hi22';
+    const link = 'hi111';
+    const attr = 'hi222222';
+    const memo = 'hi333333';
+ 
+  
+
 
   //event 객체에 접근
   evt = evt || window.event;
-  var x = 0,
-      y = 0;
-
 
   //event 객체가 pageX 속성을 포함하고 있다면 pageX와 pageY 위치
   if(evt.pageX){
@@ -34,20 +54,38 @@ function ScreenClick(evt){
 
     x = evt.clientX + offsetX;
     y = evt.clientY + offsetY;
+    
+    
   }
   console.log(x,y) //콘솔에 좌표 찍기
+
+ 
+  //DB저장
+  axios.post("/bookmarks/3",{
+    'email': email,
+    'link': link,
+    'attr': attr,
+    'memo': memo,
+    'x_coor': x,
+    'y_coor': y,
+})
+.catch(function(error) {
+    console.log(error.response);
+});
+
 
   //document.write("좌표 x=" + x + " y=" + y); //페이지 넘겨서 출력
 
 }
 
 const Stardot = () => {
-    //Client기준 좌표
+ //Client기준 좌표
 
   return (
     <div>
         <div className="Starback">
         <p>Client X, Y</p>
+    
       </div>
     </div>
   );
