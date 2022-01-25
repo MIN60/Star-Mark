88ef65cd7d5a3615c4cff23f6ad003cf32c88446
Jongjunp <56390530+Jongjunp@users.noreply.github.com>
2022-01-25 14:43:02 +0900

    mypage connection complete

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
 import { Intro } from './Intro/Intro.js';
+import { Bookmark } from './bookmark/bookmark.js';
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 
 function App() {
@@ -9,9 +11,20 @@ function App() {
     <div className="App">
       <div>
         <Router>
-          <Routes>
-            <Route exact path='/' element={<Intro />}></Route>
-          </Routes>
+          <TransitionGroup>
+            <CSSTransition
+              key={window.location.key}
+              classNames="next"
+              timeout={700}
+            >
+
+              <Routes>
+                <Route exact path='/' element={<Intro />}></Route>
+                <Route exact path='/mypage/:id' element={<Bookmark />}></Route>
+              </Routes>
+
+            </CSSTransition>
+          </TransitionGroup>  
         </Router>
       </div>
     </div>    
