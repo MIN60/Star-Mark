724b0fb483bc54aaff5b0e406fb2f78d5cb77bca
metamong-Hi <seojinlee27@naver.com>
2022-01-26 15:52:43 +0900

    fix login

--- src/component/Sidebar.js
+++ src/component/Sidebar.js
@@ -1,86 +1,48 @@
 import React from "react";
 import "./sidebar.css";
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
+import { useParams } from "react-router-dom";
+import axios from "axios";
 import { Cookies } from "react-cookie";
-
 
 ////////
 const Sidebar = () => {
-    
   const onDragStart = (event, nodeType) => {
     event.dataTransfer.setData("application/reactflow", nodeType);
     event.dataTransfer.effectAllowed = "move";
   };
 
   const { id } = useParams();
-  let gotnickname = '';
+  let gotnickname = "";
 
   const cookies = new Cookies();
 
-  axios.get("/api/users/finduser", {
-    params: { email: id },
-    headers: {
-      "Authorization": cookies.get("Authorization"),
-    },
-  }).then(function (response) {
-    console.log(response);
-    gotnickname = response.data.nickname;
-    console.log(gotnickname);
-    }).catch(function (error) {
-    return alert("Error: Can't find nickname");
-  });
+  axios
+    .get("/api/users/finduser", {
+      params: { email: id },
+      headers: {
+        Authorization: cookies.get("Authorization"),
+      },
+    })
+    .then(function (response) {
+      console.log(response);
+      gotnickname = response.data.nickname;
+      console.log(gotnickname);
+    })
+    .catch(function (error) {
+      return alert("Error: Can't find nickname");
+    });
 
   return (
     <aside className="sidebarstyle">
-      <div className="userintroduction">{`${gotnickname}'s\nBookmark Sky`}</div>
-      <div className="description" className="dndtitle">Select StarMark</div>
-      <div
-        className="dndnode"
-        onDragStart={(event) => onDragStart(event, "default")}
-        draggable
-      >
-        <img alt="star1" className="imgsize" src="img/stars.png"/>
+      <div className="description" className="dndtitle">
+        Select StarMark
       </div>
-
       <div
         className="dndnode"
         onDragStart={(event) => onDragStart(event, "default")}
         draggable
       >
-        <img alt="star1" className="imgsize" src="img/stars.png"/>
-      </div>
-
-      <div
-        className="dndnode"
-        onDragStart={(event) => onDragStart(event, "default")}
-        draggable
-      >
-        <img alt="star1" className="imgsize" src="img/stars.png"/>
-      </div>
-
-      <div
-        className="dndnode"
-        onDragStart={(event) => onDragStart(event, "default")}
-        draggable
-      >
-        <img alt="star1" className="imgsize" src="img/stars.png"/>
-      </div>
-
-      <div
-        className="dndnode"
-        onDragStart={(event) => onDragStart(event, "default")}
-        draggable
-      >
-        <img alt="star1" className="imgsize" src="img/stars.png" />
-      </div>
-
-      <div
-        className="dndnode"
-        onDragStart={(event) => onDragStart(event, "default")}
-        draggable
-      >
-        <img alt="star1" className="imgsize" src="img/stars.png"/>
+        <img alt="star1" className="imgsize" src="./img/stars.png" />
       </div>
     </aside>
   );
